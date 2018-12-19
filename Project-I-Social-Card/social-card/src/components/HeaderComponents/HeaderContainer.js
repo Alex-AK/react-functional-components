78b434c20343a5252b0a23ffd4ff7172b7a974ae
Kingaa6 <Kingaa6@gmail.com>
2018-12-19 13:03:14 -0800

    HeaderContainer finished

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderContainer.js
@@ -4,13 +4,17 @@ import './Header.css';
 
 import HeaderTitle from './HeaderTitle';
 import HeaderContent from './HeaderContent';
+import ImageThumbnail from './ImageThumbnail';
 
 
 const HeaderContainer = () => {
   return (
-    <div>
-      <HeaderTitle title="Lambda School" subtitle="@LambdaSchool · 26 jan" />
-      <HeaderContent content="Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simples and have fun. Once you feel comfortable using components you are well on your way to mastering React!" />
+    <div className="header-container">
+      <ImageThumbnail />
+      <div className="header-text">
+        <HeaderTitle title="Lambda School" subtitle="@LambdaSchool · 26 jan" />
+        <HeaderContent content="Let's learn React by building simple interfaces with components. Don't try to overthink it, just keep it simples and have fun. Once you feel comfortable using components you are well on your way to mastering React!" />
+      </div>
     </div>
   )
 }
--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,8 @@
 import React from 'react';
 import './Header.css';
+import logo from './lambda-logo.jpg';
+
+
+const ImageThumbnail = () => <img className="image-thumbnail" src={logo} alt="Lambda School Logo" />
+
+export default ImageThumbnail;
