9f717249257c382d9faf97510b4b5fc2b16dc43a
Kingaa6 <Kingaa6@gmail.com>
2018-12-23 21:17:42 -0800

    Starting project, setting up initial component connections

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+
+const CalculatorDisplay = props => {
+  return (
+    <div className="main-display">
+      <p>test</p>
+    </div>
+  )
+};
+
+
+export default CalculatorDisplay;
